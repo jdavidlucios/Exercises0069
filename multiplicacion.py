da21b28a27018ecfb79421d64c1b45b5a485cd10
jdavidlucios <juandalucio@gmail.com>
2024-08-12 21:17:42 -0400

    added first python exercise

---
+++ multiplicacion.py
@@ -0,0 +1,10 @@
+# Asignamos los valores 20 y 30 a las variables "a" y "b", respectivamente
+a = 20  # Variable "a" con valor 20
+b = 30  # Variable "b" con valor 30
+
+# Realizamos la multiplicación de las variables "a" y "b"
+# y almacenamos el resultado en la variable "resultado"
+resultado = a * b  # Multiplicación de "a" y "b"
+
+# Imprimimos el resultado de la multiplicación en pantalla
+print(f"El resultado de la multiplicación de {a} y {b} es: {resultado}")  # Impresión del resultado
