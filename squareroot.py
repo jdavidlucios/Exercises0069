3163409ada8b52b6f21b096cfe7d3020a0a84a3c
jdavidlucios <juandalucio@gmail.com>
2024-08-12 21:52:40 -0400

    added squareroot exercise

---
+++ squareroot.py
@@ -0,0 +1,12 @@
+# Importamos el módulo math para utilizar la función sqrt()
+import math
+
+# Asignamos el valor 81 a la variable "y"
+y = 81
+
+# Calculamos la raíz cuadrada de la variable "y" utilizando la función sqrt()
+# y asignamos el resultado a la variable "raiz_cuadrada"
+raiz_cuadrada = math.sqrt(y)
+
+# Imprimimos el resultado en pantalla
+print("La raíz cuadrada de", y, "es:", raiz_cuadrada)
