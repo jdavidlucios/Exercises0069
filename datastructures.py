aced786a27f85c66d4553751d0e315d79c48dc5c
jdavidlucios <juandalucio@gmail.com>
2024-08-26 21:33:53 -0400

    data structures exercise

---
+++ datastructures.py
@@ -0,0 +1,9 @@
+mi_lista = [5, 20, 15, 20, 25, 50, 20, 5, 18, 15]
+
+# Eliminar elementos duplicados
+mi_lista_sin_duplicados = list(set(mi_lista))
+
+# Ordenar la lista en orden ascendente
+mi_lista_ordenada = sorted(mi_lista_sin_duplicados)
+
+print(mi_lista_ordenada)
